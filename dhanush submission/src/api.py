df5e9dba13337d5a064cba9d9974ac63458b41ef
Dhanush Kumar <shankar.dh@northeastern.edu>
2024-04-01 12:15:36 -0400

    fixed path variable

--- dhanush submission/src/api.py
+++ dhanush submission/src/api.py
@@ -17,8 +17,9 @@ class Query(BaseModel):
 @app.post("/process_query/")
 async def process_query(query: Query):
     try:
-        file_obj = utils.load_data_from_s3(os.getenv("BUCKET_NAME"), 'processed_data.csv')
-        df = pd.read_csv(file_obj)
+        # file_obj = utils.load_data_from_s3(os.getenv("BUCKET_NAME"), 'processed_data.csv')
+        # df = pd.read_csv(file_obj)
+        df = pd.read_csv('../data/processed_data.csv')
         extracted_text, extracted_urls, cosine_scores = find_relevant_docs(query.query, df)
         response = generate_response(query.query, extracted_text)
         return {"response": response, "extracted_documents": extracted_text, "urls": extracted_urls, "cosine_scores": cosine_scores}
